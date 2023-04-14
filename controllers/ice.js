c564e01e15c5d56dad52108cfb095c6b23d67d8b
kevin harris <kcharris77@gmail.com>
2023-04-14 01:17:01 -0500

    Add ice detail get for id

--- controllers/ice.js
+++ controllers/ice.js
@@ -12,9 +12,17 @@ exports.ice_list = async function(req, res) {
 };
         
 // for a specific ice.
-exports.ice_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Ice detail: ' + req.params.id);
-};
+exports.ice_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Ice.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle Ice create on POST.
 exports.Ice_create_post = async function(req, res) {
     console.log(req.body)
