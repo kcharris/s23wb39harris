430bea6c408ed4d44c9abecb2849c3967b4126e0
kevin harris <kcharris77@gmail.com>
2023-04-27 22:13:00 -0500

    Add code through problems 4 and 5

--- routes/ice.js
+++ routes/ice.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const ice_controlers= require('../controllers/ice');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 /* GET ices */
 router.get('/', ice_controlers.ice_view_all_Page );
 
@@ -13,7 +23,7 @@ router.get('/detail', ice_controlers.ice_view_one_Page);
 /* GET create ice page */
 router.get('/create', ice_controlers.ice_create_Page);
 /* GET create update page */
-router.get('/update', ice_controlers.ice_update_Page);
+router.get('/update',secured, ice_controlers.ice_update_Page);
 /* GET delete ice page */
 router.get('/delete', ice_controlers.ice_delete_Page);
 
